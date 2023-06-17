39555a3456640e8007549592c5576800f787dc02
Anna Góralik <a.goralik02@gmail.com>
2023-06-17 11:19:26 +0200

    gps and description added

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+let weatherForm = document.querySelector('.weather__form')
+let cityInput = document.querySelector('.weather__city')
+let apiUrl = 'https://api.weatherapi.com/v1/current.json?key=55485f5c20ea419db0a165500231506&aqi=no&q='
+let apiDataContainer = document.querySelector('.weather__data')
+let loader = document.querySelector('.weather__loader')
+let gpsButton = document.querySelector('.weather__gps')
+
+weatherForm.addEventListener('submit', (event) => {
+    showLoader()
+    let city = cityInput.value
+    let fullApiUrl= apiUrl + city
+   
+    fetch(fullApiUrl)
+        
+        .then(response => {
+            hideLoader()
+            if(response.status === 200){
+                return response.json()
+            }
+
+            throw new Error()
+        })
+        .then((dataFromApi) => {
+            // console.log(dataFromApi.current.temp_c)
+            let view = ``
+            // view += `Dzisiaj w ${dataFromApi.location.name} jest ${dataFromApi.current.temp_c} &deg;C`
+            // view += `<img src="${dataFromApi.current.condition.icon}" alt="${dataFromApi.current.condition.text}">`
+            view += `<div class="weather__info">`
+        // county, city, time(region)
+        view += `<div class="weather__region"> 
+                <p>${dataFromApi.location.name}</p>
+                <p>${dataFromApi.location.country}</p>
+                <p> ${dataFromApi.location.localtime}</p>
+        </div>`
+ 
+        //icon
+        view += `<div class="weather__icon"><img src= ${dataFromApi.current.condition.icon} 
+        alt = ${dataFromApi.current.condition.text}></div >`
+        //temp
+        view += `<div class="weather__temp"> ${dataFromApi.current.temp_c} <span>&degC</span></div >`
+ 
+        //details
+        view += `<div class="weather__details">
+                <p>The amount of rainfall: ${dataFromApi.current.precip_mm}mm </p>
+                <p>Humidity: ${dataFromApi.current.humidity}%</p>
+                <p>Wind: ${dataFromApi.current.wind_kph}km/h</p>
+            </div>`
+ 
+        view += `</div>`
+            apiDataContainer.innerHTML = view 
+        }).catch ((error) => showError())
+        
+    event.preventDefault()
+
+})
+
+let showError = () => {
+    apiDataContainer.innerHTML = `<div class="weather__error">City not found or we have problem with API</div>`
+}
+
+let showLoader = () => {
+    loader.style.display = 'block'
+}
+
+let hideLoader = () => {
+    loader.style.display = 'none'
+}
+
+// gps
+
+gpsButton.addEventListener('click', (event) => {
+    navigator.geolocation.getCurrentPosition(searchPosition)
+})
+
+let searchPosition = (position) => {
+    // console.log(position)
+    cityInput.value = `${position.coords.latitude},${position.coords.longitude}`
+    weatherForm.requestSubmit()
+}
